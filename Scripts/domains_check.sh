ed0dc4e68237181ced86621ccadb8fc242b57e7c
TECH SCOUT <techsc0utOP@gmail.com>
2022-03-17 13:22:34 +0530

    Create domains_check.sh

---
+++ Scripts/domains_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+filename='paytm_subd.txt'
+for hstnm in $(cat file1.txt)
+do
+if ( dig @8.8.8.8 -t A "$hstnm" | grep -q 'NXDOMAIN' ) || ( dig @8.8.8.8 -t A "$hstnm" | grep -q 'SERVFAIL' ) && ( dig @8.8.8.8 -t CNAME "$hstnm" +short >/dev/null )
+then
+echo -n -e "\e[92mNXDOMAIN: \e[0m${hstnm} "
+echo -n -e "\e[93mCNAME: \e[0m"; dig @8.8.8.8 -t CNAME "$hstnm" +short
+echo
+else
+echo "${hstnm}" >> $filename
+fi
+done
